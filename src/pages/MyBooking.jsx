d04dc4ea65273ebea5c6da447d9fc32ab5e1cb5b
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2025-11-11 22:49:48 +0600

    added

--- src/pages/MyBooking.jsx
+++ src/pages/MyBooking.jsx
@@ -1,5 +1,6 @@
 import React, { use, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import Swal from "sweetalert2";
 
 const MyBooking = () => {
   const { users } = use(AuthContext);
@@ -14,6 +15,37 @@ const MyBooking = () => {
     }
   }, [users]);
 
+  const handleCancel = (_id) => {
+    console.log("e");
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/bookings/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("delete", data);
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Cancel!",
+                text: "Your booking has been cancel.",
+                icon: "success",
+              });
+              const remainingBooks = books.filter(book=>book._id !== _id)
+              setBooks(remainingBooks)
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="overflow-x-auto md:w-7xl mx-auto py-30">
       <table className="table">
@@ -47,7 +79,12 @@ const MyBooking = () => {
               <td>{book.date}</td>
               <td>{book.price}</td>
               <th>
-                <button className="btn btn-primary">Cancel</button>
+                <button
+                  onClick={() => handleCancel(book._id)}
+                  className="btn btn-primary"
+                >
+                  Cancel
+                </button>
               </th>
             </tr>
           ))}
